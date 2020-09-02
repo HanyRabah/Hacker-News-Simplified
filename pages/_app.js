2e31932ce57b1131b86058b6a3fff955281a0be4
Hany <hany.rabah@gmail.com>
2020-09-02 09:46:52 +0400

    adjusting app and index pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Layout from "@/components/Layout";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, theme } from "@/styles/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
