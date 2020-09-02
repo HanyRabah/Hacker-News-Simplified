c5c7331b05ca02ab7bb3feaba8fc5bafa58feba4
Hany <hany.rabah@gmail.com>
2020-09-02 12:07:38 +0400

    adjust global theme

--- styles/theme.js
+++ styles/theme.js
@@ -17,24 +17,7 @@ export const GlobalStyle = createGlobalStyle`
 
 export const theme = {
   color: {
-    white: "#ffffff",
-    black: "#000000",
-    dark_gray: "#666666",
-    gray: "#424242",
-    light_gray: "#eff3f6",
-    green: "#3cb371",
-    dark_green: "#348a5a",
-    red: "red",
-    alert_text: "#721c24",
-    alert_background: "#f8d7da",
-    alert_border: "#f5c6cb",
-    info_text: "#856404",
-    info_background: "#fff3cd",
-    info_border: "#ffeeba",
-    yellow: "#ffc107",
-    link: "#007db5",
-    link_hover: "linear-gradient(90deg,#ff8a00,#e52e71)",
-    text_color: '#aaa'
+    orange: "#FF6C47",
   },
   fontSize: {
     tiny: '10px',
